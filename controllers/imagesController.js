64287772ced5a2d853986bfb775ff1304cb1c9ec
ShobiyaL <sofy7468612@gmail.com>
2023-12-06 19:44:23 +0530

    lk

--- controllers/imagesController.js
+++ controllers/imagesController.js
@@ -1,12 +1,16 @@
 import { db } from '../db.js';
 
 export const uploadsFunc = (req, res) => {
+  console.log(req.file, 'file....');
+  console.log(req.body, 'body data......');
   console.log(req.user.id);
-  const { description } = req.body;
+  const { description, image } = req.body;
   console.log(description);
-  const imageUrl = req.file.buffer.toString('base64');
-  console.log(imageUrl);
-
+  // const imageUrl = image.toString('base64');
+  // // const imageContent = Buffer.from(imageUrl, 'base64');
+  // console.log(imageUrl, 'image | imageurl');
+  // // console.loh(imageContent);
+  const imageUrl = req.file.filename;
   const q = 'INSERT INTO `images` (`description`,`img`,`uid`) VALUES (?,?,?)';
 
   db.query(q, [description, imageUrl, req.user.id], (err, data) => {
