d2eb86d8653c8e0e7662e57daff54a33318cadff
ShobiyaL <sofy7468612@gmail.com>
2023-12-06 08:11:27 +0530

    o

--- controllers/imagesController.js
+++ controllers/imagesController.js
@@ -1,13 +1,13 @@
 import { db } from '../db.js';
 
 export const uploadsFunc = (req, res) => {
-  // console.log(req.user.id);
+  console.log(req.user.id);
   const { description } = req.body;
-  // console.log(description);
+  console.log(description);
   const imageUrl = req.file.filename;
-  // console.log(imageUrl);
+  console.log(imageUrl);
 
-  const q = 'INSERT INTO images (`description`,`img`,`uid`) VALUES (?,?,?)';
+  const q = 'INSERT INTO `images` (`description`,`img`,`uid`) VALUES (?,?,?)';
 
   db.query(q, [description, imageUrl, req.user.id], (err, data) => {
     if (err) return err;
@@ -20,7 +20,7 @@ export const uploadsFunc = (req, res) => {
 
 export const getImages = async (req, res) => {
   // console.log(req.user.id);
-  const q = 'SELECT * FROM images WHERE uid = ?';
+  const q = 'SELECT * FROM `images` WHERE `uid` = ?';
   db.query(q, [req.user.id], (err, data) => {
     if (err) {
       console.log(err);
