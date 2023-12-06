9c62f596cbc8a3b9548bf9c9a1ecd0f462a46e0d
ShobiyaL <sofy7468612@gmail.com>
2023-12-06 14:04:02 +0530

    lkj12wj

--- routes/imageRoutes.js
+++ routes/imageRoutes.js
@@ -10,9 +10,7 @@ const __dirname = dirname(__filename);
 const router = express.Router();
 
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, path.join(__dirname, 'public', 'images'));
-  },
+  destination: 'public/images',
   filename: (req, file, cb) => {
     console.log(file);
     cb(
