cd8d90af67534824a8e3c4b8c2f64b7326317429
ShobiyaL <sofy7468612@gmail.com>
2023-12-06 13:30:09 +0530

    lkj12wl09

--- routes/imageRoutes.js
+++ routes/imageRoutes.js
@@ -5,7 +5,7 @@ import { uploadsFunc, getImages } from '../controllers/imagesController.js';
 const router = express.Router();
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, '/public/images');
+    cb(null, 'public/images');
   },
   filename: (req, file, cb) => {
     console.log(file);
